79c869a9d27c41d8f4e13469f70548c851cdd38a
Le_redstonneur <50110370+LeRedstonneur@users.noreply.github.com>
2023-02-14 16:17:02 +0100

    Séparations en classes + bouton pour quitter

--- code/niveau1/main.py
+++ code/niveau1/main.py
@@ -1,84 +1,75 @@
 import pygame
+from player import Player
+from quit import quit
+
 
 def start():
-    print("On ouvre la fenêtre")
+    '''Cette fonction est appelée par l'écran d'accueil pour démarrer le niveau'''
     pygame.init()
-    width = 800
-    height = 600
-    screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
+    
+    
+
+    # Gestion de l'écran
+    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
     bg = pygame.image.load("map.png")
-    pygame.display.set_caption("Pygame Example")
-    screen.fill((255, 255, 255))
-    pygame.display.update()
-    bg = pygame.transform.scale(bg, (1280, 720))
     rect = bg.get_rect()
 
-    # Par défaut, le joueur est immobile
-    mouvements = {"jump": False, "left": False, "right": False, "up": False, "down": False}
+    # Le titre de la fenêtre
+    pygame.display.set_caption("Virus Gate")
 
-    posx = 0
-    posy = 0
-    speed = 2.5
+    # L'objet Joueur
+    joueur = Player()
+
+    # On récupère les informations de l'écran
+    screen_info = pygame.display.Info()
+    w, h = screen_info.current_w, screen_info.current_h
+
+
+    # définition des dimensions du bouton
+    largeur_bouton = 100
+    hauteur_bouton = 50
+
+
+    # création de la police puis du texte du bouton
+    texte_bouton = pygame.font.Font(None, 24).render("X", True, (255, 255, 255))
+    rectangle_bouton = pygame.Rect(100, 100, largeur_bouton, hauteur_bouton)
+    transparent_surface = pygame.Surface((largeur_bouton, hauteur_bouton), pygame.SRCALPHA)
+
+    # Remplir la surface transparente avec une transparence totale
+    transparent_surface.fill((0, 0, 0, 0))
+    transparent_surface.blit(texte_bouton, (0, 0))
+    pygame.draw.rect(transparent_surface, (0, 0, 0, 0), transparent_surface.get_rect(), 1)
+
+    obstacles = [pygame.Rect(1000, 1000, 50, 50)]
 
     running = True
     while running:
+        maj = pygame.event.get()
 
-        if mouvements["left"]:
-            posx += speed
-        if mouvements["right"]:
-            posx -= speed
-        if mouvements["up"]:
-            posy += speed
-        if mouvements["down"]:
-            posy -= speed
-
-        print(f"x = {posx}    y = {posy}")
-
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    mouvements["jump"] = True
-                if event.key == pygame.K_LEFT:
-                    mouvements["left"] = True
-                if event.key == pygame.K_RIGHT:
-                    mouvements["right"] = True
-                if event.key == pygame.K_UP:
-                    mouvements["up"] = True
-                if event.key == pygame.K_DOWN:
-                    mouvements["down"] = True
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_SPACE:
-                    mouvements["jump"] = False
-                if event.key == pygame.K_LEFT:
-                    mouvements["left"] = False
-                if event.key == pygame.K_RIGHT:
-                    mouvements["right"] = False
-                if event.key == pygame.K_UP:
-                    mouvements["up"] = False
-                if event.key == pygame.K_DOWN:
-                    mouvements["down"] = False
-
+        for event in maj:
             if event.type == pygame.QUIT:
                 running = False
                 quit()
-            elif event.type == pygame.VIDEORESIZE:
-                width, height = event.size
-                screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
+                return  # On return pour quitter la fonction start
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                if rectangle_bouton.collidepoint(event.pos):
+                    running = False
+                    quit()
+                    return  # On return pour quitter la fonction start
 
-        #background_image = pygame.transform.scale(background_image, (width, height))
-        #screen.blit(background_image, (0, 0))
-        #screen.blit(bg, (0, 0))
         screen.fill((255, 255, 255))
         rect = rect.move(0, 0)
         screen.blit(bg, rect)
 
-        pygame.draw.rect(screen, 'BLUE',(-posx,-posy,30,30))
+        # On affiche la croix à l'écran
+        screen.blit(transparent_surface, rectangle_bouton)
 
+        # On affiche les obstacles
+        for element in obstacles:
+            pygame.draw.rect(screen, (0, 0, 0), element)
+
+        joueur.update(maj, screen, obstacles)
+        
         pygame.display.update()
 
-def quit():
-    print("On quitte le programme")
-    pygame.quit()
-
 start()
-
