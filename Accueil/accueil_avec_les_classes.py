35f9b93e7eceae1fb2c31fe99f45be0b0f1c86d5
Xerondo <72127675+Xerondo@users.noreply.github.com>
2023-03-28 23:12:14 +0200

    ajout de la première bande son

--- Accueil/accueil_avec_les_classes.py
+++ Accueil/accueil_avec_les_classes.py
@@ -1,11 +1,27 @@
 import pygame
+print(pygame.ver)
 
 pygame.init()
 pygame.mixer.init()
 
 # Obtenir la résolution d'écran
 screen_info = pygame.display.Info()
-width, height = screen_info.current_w, screen_info.current_h
+screen_width, screen_height = screen_info.current_w, screen_info.current_h
+
+# Charger les image de fond
+menu_bg = pygame.image.load("Fond d'ecran accueil.jpg")
+options_bg = pygame.image.load("Fond d'ecran accueil.jpg")
+
+# Récupérer la taille de l'image de fond
+menu_bg_width, menu_bg_height = menu_bg.get_size()
+options_bg_width, options_bg_height = options_bg.get_size()
+
+# Calculer les positions pour centrer l'image
+menu_bg_x = (screen_width - menu_bg_width) // 2
+menu_bg_y = (screen_height - menu_bg_height) // 2
+options_bg_x = (screen_width - options_bg_width) // 2
+options_bg_y = (screen_height - options_bg_height) // 2
+
 
 # Définir les couleurs
 WHITE = (255, 255, 255)
@@ -52,101 +68,133 @@ class Button:
         return self.rect.collidepoint(mouse_pos)
 
 # Définir les dimensions et l'emplacement du premier bouton
-button1 = Button("Niveau 1", 300, 100, 50, height / 1.3 - 100 / 1.3, BLUE_HACKER, BLACK)
+button1 = Button("Niveau 1", 300, 100, 50, screen_height / 1.3 - 100 / 1.3, BLUE_HACKER, BLACK)
 
 # Définir les dimensions et l'emplacement du second boutton
-button2 = Button("Niveau 2", 300, 100, 50, width / 2 - 100 / 2, BLUE_HACKER, BLACK)
+button2 = Button("Niveau 2", 300, 100, 50, screen_height / 1.1 - 100 / 1.3, BLUE_HACKER, BLACK)
 
 # Définir les dimensions et l'emplacement du bouton option
-options = Button("Options", 200, 70, 1475, height / 1.05 - 70 / 1.05, GRAY, BLACK)
+options = Button("Options", 300, 100, screen_width - 350, screen_height / 1.3 - 100 / 1.3, GRAY, BLACK)
 
 # Définir les dimensions et l'emplacement du bouton quitter
-quitter = Button("Quitter", 200, 70, 1700, height / 1.05 - 70 / 1.05, GRAY, BLACK)
+quitter = Button("Quitter", 300, 100, screen_width - 350, screen_height / 1.1 - 100 / 1.3, GRAY, BLACK)
 
 # Définir les dimensions et l'emplacement du bouton retour (dans les options)
-retour = Button("Retour", 200, 70, 1000, width / 2 - height / 1.5, GRAY, BLACK)
+retour = Button("Retour", 300, 100, 1000, screen_width / 2 - screen_height / 1.5, GRAY, BLACK)
 
 # Créer la fenêtre en mode plein écran
-screen = pygame.display.set_mode((width, height), pygame.FULLSCREEN)
+screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN)
 
 # Initialisation de la variable pour vérifier si nous sommes dans le menu principal
 in_menu = True
 in_options = False
 
-def barre_volume(volume):
+def barre_volume():
     global volume_bande_son_accueil
     global barre_volume_bande_son
     global barre_volume_bande_son_rect
     global curseur_volume_bande_son
     global curseur_volume_bande_son_rect
 
-    volume_bande_son_accueil = volume
-
     # Mettre à jour la barre de progression du volume de la bande son
-    barre_volume_bande_son = pygame.Surface((int(volume_bande_son_accueil * 5000), 50))
+    barre_volume_bande_son = pygame.Surface((1000, 50))
     barre_volume_bande_son.fill((0, 255, 0))  # couleur verte
     barre_volume_bande_son_rect = barre_volume_bande_son.get_rect()
-    barre_volume_bande_son_rect.x = 300
-    barre_volume_bande_son_rect.y = 200
+    barre_volume_bande_son_rect.x = screen_height / 6
+    barre_volume_bande_son_rect.y = screen_height / 2
 
     # Curseur pour régler le volume de la bande son
-    curseur_volume_bande_son = pygame.Surface((500, 50))
+    curseur_volume_bande_son = pygame.Surface((10, 50))
     curseur_volume_bande_son.fill((0, 0, 0))  # couleur noire
     curseur_volume_bande_son_rect = curseur_volume_bande_son.get_rect()
-    curseur_volume_bande_son_rect.x = int(300 + volume_bande_son_accueil * 200 - 10)
-    curseur_volume_bande_son_rect.y = 200
+    curseur_volume_bande_son_rect.y = screen_height / 2
 
-    # Réduire le volume de la bande son en fonction du réglage
-    son_accueil.set_volume(volume)
 
 def afficher_options():
-    global in_options, in_menu
-    global return_rect
-    global options_page
+    global in_menu, in_options, running
+    global volume_bande_son_accueil
 
-    # Si l'utilisateur clique sur le bouton "Options", changez la valeur de "in_options" à True
-    in_options = True
-    in_menu = False
+    barre_volume()
+    # Modifier ces variables globales permet de les utiliser dans d'autres fonctions
+    global barre_volume_bande_son
+    global barre_volume_bande_son_rect
+    global curseur_volume_bande_son
+    global curseur_volume_bande_son_rect
 
-    # Si "in_options" est True, afficher les éléments que vous souhaitez afficher pour la page "Options"
-    screen.blit(background_image,(0,0))
-    son_accueil.set_volume(0.1)
+    while in_options:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
 
-    options_page = pygame.Surface((width, height))
-    options_page.fill(WHITE)
-    options_title = font.render("Options", True, BLACK)
-    options_title_rect = options_title.get_rect(center=(width / 2, height / 2.6))
-    options_page.blit(options_title, options_title_rect)
+            if event.type == pygame.KEYDOWN:
+                # Vérifier si l'utilisateur a cliqué sur le bouton "Echap"
+                if event.key == pygame.K_ESCAPE:
+                    in_options = False
+                    in_menu = True
 
-    # Afficher le bouton retour à l'écran      
-    screen.blit(retour.surface, retour.rect)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:  # 1 pour le bouton gauche de la souris
+                    # Vérifier si l'utilisateur a cliqué sur le bouton "retour"
+                    if retour.is_clicked(event.pos):
+                        in_options = False
+                        in_menu = True
 
-    # Appeler la fonction barre_volume après la création du curseur et avant l'affichage de la page
-    barre_volume(0.1)
-    options_page.blit(curseur_volume_bande_son, curseur_volume_bande_son_rect)
-    options_page.blit(barre_volume_bande_son, barre_volume_bande_son_rect)    
-    screen.blit(options_page, (0, 0))
+                    if quitter.is_clicked(event.pos):
+                        in_options = False
+                        in_menu = True
+                        running = False
 
-    pygame.display.flip()
+                    # Vérifier si l'utilisateur a cliqué sur la barre de progression du volume de la bande son
+                    if barre_volume_bande_son_rect.collidepoint(event.pos):
+                        # Régler le volume de la bande son en fonction de la position du curseur
+                        volume_bande_son_accueil = ((event.pos[0] - (barre_volume_bande_son_rect.x)) / 1000)
+                        son_accueil.set_volume(volume_bande_son_accueil)
+
+        # Afficher l'image de fond
+        screen.blit(options_bg, (options_bg_x, options_bg_y))
+
+        # Afficher les boutons
+        screen.blit(quitter.surface, quitter.rect)
+        screen.blit(retour.surface, retour.rect)
+
+        # Afficher le titre "Options"
+        font = pygame.font.Font("freesansbold.ttf", 50)
+        titre_surface = font.render("Options", True, GRAY)
+        titre_rect = titre_surface.get_rect(center=(screen_width / 2, screen_height / 8))
+        screen.blit(titre_surface, titre_rect)
+
+        # Afficher le texte "Volume de la bande son"
+        texte_volume_bande_son = font.render("Volume de la bande son", True, BLACK)
+        texte_volume_bande_son_rect = texte_volume_bande_son.get_rect(center=(screen_width / 2, screen_height / 2 - 50))
+        screen.blit(texte_volume_bande_son, texte_volume_bande_son_rect)
+
+        # Afficher la barre de progression du volume de la bande son
+        screen.blit(barre_volume_bande_son, barre_volume_bande_son_rect)
+
+        # Afficher le curseur à propos du volume
+        curseur_volume_bande_son_rect.x = int((barre_volume_bande_son_rect.x) + volume_bande_son_accueil * 1000)
+        screen.blit(curseur_volume_bande_son, curseur_volume_bande_son_rect)
+
+        pygame.display.update()
+
 
 def afficher_menu():
     global in_options
     global in_menu, in_options
-    global background_image
+    global menu_bg
 
     in_options = False
     in_menu = True
 
-    # Charger l'image de fond
-    background_image = pygame.image.load("Fond d'ecran accueil.jpg")
-    options_bg = pygame.image.load("Fond d'ecran accueil.jpg")
-    screen.blit(options_bg, (0, 0))
+    # Afficher le bg
+    screen.blit(menu_bg, (menu_bg_x, menu_bg_y))
 
     # Définir la police et la couleur du texte
     font = pygame.font.Font("freesansbold.ttf", 150)
     text = font.render("Virus Gate", True, (GRAY))
     text_rect = text.get_rect()
-    text_rect.center = (width // 2, height // 9)  # Centrer le texte dans la fenêtre
+    text_rect.center = (screen_width // 2, screen_height // 9)  # Centrer le texte dans la fenêtre
     screen.blit(text, text_rect)
 
     # Afficher les boutons à gauche de l'écran      
@@ -167,16 +215,17 @@ while running:
             if event.button == 1:  # 1 pour le bouton gauche de la souris
                 if in_menu:
                     if options.is_clicked(event.pos): # Si l'utilisateur appuie sur "options", accède à la partie "options"
-                        afficher_options() 
+                        in_options = True
+                        in_menu = False
                     elif quitter.is_clicked(event.pos): # Si l'utilisateur appuie sur "quitter", le jeu s'arrête
-                        running = False 
+                        running = False
                 elif in_options:
                     if retour.is_clicked(event.pos): # Si l'utilisateur clique sur le bouton "Retour", changez la valeur de "in_options" à False
                         in_options = False 
-
-    if in_options:
+                        in_menu = True
+    if in_options :
         afficher_options()
-    else:
+    elif in_menu :
         afficher_menu()
 
     # Mettre à jour l'affichage
