b742dc344cb2aa0c0e21e4eff90e1d2b054c441b
SLeotch <123469709+SLeotch@users.noreply.github.com>
2023-04-04 15:49:55 +0200

    Update obstacles.py

--- Chapitre_2/Code/obstacles.py
+++ Chapitre_2/Code/obstacles.py
@@ -9,7 +9,7 @@ pygame.Rect(1350, 935, 60, 20), pygame.Rect(1350, 760, 60, 20), pygame.Rect(1750
 pygame.Rect(2300, 910, 250, 100), pygame.Rect(2300, 680, 250, 120), pygame.Rect(2200, 960, 50, 50), pygame.Rect(2250, 910, 50, 100), pygame.Rect(2400, 640, 150, 50),
 pygame.Rect(2400, 590, 150, 50), pygame.Rect(2625, 830, 50, 200), pygame.Rect(2800, 830, 50, 180), pygame.Rect(2975, 830, 50, 180), pygame.Rect(3200, 250, 150, 20),
 pygame.Rect(3200, 1000, 80, 20), pygame.Rect(3400, 1000, 80, 20), pygame.Rect(3600, 1000, 80, 20),pygame.Rect(3800, 1000, 80, 20), pygame.Rect(4000, 1000, 80, 20), 
-pygame.Rect(3400, 400, 150, 20), pygame.Rect(3400, 550, 150, 20), pygame.Rect(3600, 300, 150, 20), pygame.Rect(3900, 650, 520, 20), pygame.Rect(4600, 1010, 3000, 500),
+pygame.Rect(3400, 400, 150, 20), pygame.Rect(3400, 550, 150, 20), pygame.Rect(3600, 300, 150, 20), pygame.Rect(3900, 650, 530, 20), pygame.Rect(4600, 1010, 3000, 500),
 pygame.Rect(5100, 910, 80, 20), pygame.Rect(4900, 850, 80, 20), pygame.Rect(5080, 760, 80, 20), pygame.Rect(5270, 690, 80, 20), pygame.Rect(5100, 590, 80, 20),
 pygame.Rect(5250, 510, 80, 20), pygame.Rect(5420, 430, 80, 20), pygame.Rect(5650, 510, 80, 20), pygame.Rect(5820, 590, 80, 20), pygame.Rect(6050, 40, 50, 750),
 pygame.Rect(5650, 690, 80, 20), pygame.Rect(5670, 840, 80, 20), pygame.Rect(5450, 620, 150, 50), pygame.Rect(5810, 870, 80, 20), pygame.Rect(5810, 740, 1250, 50),
@@ -27,3 +27,5 @@ Spike(-4200, -630), Spike(-4625, -990), Spike(-5250, -990), Spike(-5300, -990), 
 Spike(-5550, -990), Spike(-5600, -990), Spike(-5650, -990), Spike(-5700, -990), Spike(-5750, -990), Spike(-5800, -990), Spike(-5850, -990), Spike(-5900, -990),
 Spike(-5950, -990), Spike(-6000, -990), Spike(-5475, -600), Spike(-5525, -600), Spike(-5575, -600), Spike(-5835, -720), Spike(-5885, -720),Spike(-5935, -720), 
 Spike(-5985, -720), Spike(-6035, -720)]
+
+checkpoints=[(4750, 965)]
