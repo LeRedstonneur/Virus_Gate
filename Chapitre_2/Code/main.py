220df8034fa945a9d2f2de9cfb7ae7a3a870f334
Le_redstonneur <50110370+LeRedstonneur@users.noreply.github.com>
2023-04-04 10:15:39 +0200

    Déportation des obstacles
    Les obstacles sont déclarés dans un fichier séparé du code

--- Chapitre_2/Code/main.py
+++ Chapitre_2/Code/main.py
@@ -3,6 +3,10 @@ from player import Player
 from tower import Tower
 from trap import Trap
 from quit import leave
+from obstacles import obstacles
+from obstacles import towers
+from obstacles import traps
+# from obstacles import spikes
 
 
 def start():
@@ -11,7 +15,7 @@ def start():
 
     # Gestion de l'écran
     screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-    bg = pygame.image.load("./assets/a.jpg")
+    bg = pygame.image.load("../Assets/a.jpg")
     rect = bg.get_rect()
 
     # Le titre de la fenêtre
@@ -36,17 +40,9 @@ def start():
 
     clock = pygame.time.Clock()
 
-    obstacles = [pygame.Rect(1100, 950, 50, 50), pygame.Rect(0, 1010, 10000, 500), pygame.Rect(1000, 700, 50, 250), pygame.Rect(500, 950, 150, 50), 
-    pygame.Rect(660, 870, 60, 20), pygame.Rect(760, 790, 60, 20), pygame.Rect(860, 710, 60, 20), pygame.Rect(1400, 700, 250, 350), pygame.Rect(1300, 845, 60, 20),
-    pygame.Rect(1350, 935, 60, 20), pygame.Rect(1350, 760, 60, 20), pygame.Rect(1750, 650, 80, 20), pygame.Rect(2000, 850, 80, 20), pygame.Rect(2200, 780, 100, 20),
-    pygame.Rect(2300, 910, 250, 100), pygame.Rect(2300, 680, 250, 120), pygame.Rect(2200, 960, 50, 50), pygame.Rect(2250, 910, 50, 100), pygame.Rect(2400, 640, 150, 50),
-    pygame.Rect(2400, 590, 150, 50), pygame.Rect(2625, 830, 50, 200), pygame.Rect(2800, 830, 50, 180), pygame.Rect(2975, 830, 50, 180)]
+    # Les obstacles sont directement includes depuis obstacles.py
 
-    towers = [Tower("classique", -600, -600), Tower("degats_de_zone", -1000, -450), Tower("classique", -1900, -1000), Tower("degats_de_zone",-2250, -425), 
-    Tower("classique", -2740, -1000), Tower("classique", -2915, -1000)]
 
-    traps = [Trap(-800, -1000), Trap(-2225, -935), Trap(-2175, -985)]
-    #spikes = [Spike(-750, -1100)]
 
     # On récupère les informations de l'écran
     screen_info = pygame.display.Info()
