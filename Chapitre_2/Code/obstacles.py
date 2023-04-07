e681cf84d0900eb4c96aa03f448274e9492c78c5
SLeotch <123469709+SLeotch@users.noreply.github.com>
2023-04-07 14:06:10 +0200

    Update obstacles.py

--- Chapitre_2/Code/obstacles.py
+++ Chapitre_2/Code/obstacles.py
@@ -12,14 +12,20 @@ pygame.Rect(3200, 1000, 80, 20), pygame.Rect(3400, 1000, 80, 20), pygame.Rect(36
 pygame.Rect(3400, 400, 150, 20), pygame.Rect(3400, 550, 150, 20), pygame.Rect(3600, 300, 150, 20), pygame.Rect(3900, 650, 530, 20), pygame.Rect(4600, 1010, 3000, 500),
 pygame.Rect(5100, 910, 80, 20), pygame.Rect(4900, 850, 80, 20), pygame.Rect(5080, 760, 80, 20), pygame.Rect(5270, 690, 80, 20), pygame.Rect(5100, 590, 80, 20),
 pygame.Rect(5250, 510, 80, 20), pygame.Rect(5420, 430, 80, 20), pygame.Rect(5650, 510, 80, 20), pygame.Rect(5820, 590, 80, 20), pygame.Rect(6050, 40, 50, 750),
-pygame.Rect(5650, 690, 80, 20), pygame.Rect(5670, 840, 80, 20), pygame.Rect(5450, 620, 150, 50), pygame.Rect(5810, 870, 80, 20), pygame.Rect(5810, 740, 1250, 50),
-pygame.Rect(5950, 920, 120, 20), pygame.Rect(6050, 860, 50, 150)]
+pygame.Rect(5650, 690, 80, 20), pygame.Rect(5670, 840, 80, 20), pygame.Rect(5450, 620, 150, 50), pygame.Rect(5810, 870, 80, 20), pygame.Rect(5810, 740, 1150, 50),
+pygame.Rect(5950, 920, 120, 20), pygame.Rect(6050, 860, 50, 150), pygame.Rect(6250, 750, 50, 150), pygame.Rect(6500, 865, 50, 50), pygame.Rect(6400, 965, 50, 50),
+pygame.Rect(6600, 750, 50, 150), pygame.Rect(6750, 950, 80, 20), pygame.Rect(6900, 880, 80, 20), pygame.Rect(7050, 950, 80, 20), pygame.Rect(7200, 880, 80, 20), 
+pygame.Rect(7050, 820, 80, 20), pygame.Rect(7350, 350, 50, 800), pygame.Rect(6300, 500, 1050, 50), pygame.Rect(6250, 650, 80, 20), pygame.Rect(6100, 550, 80, 20),
+pygame.Rect(6380, 380, 50, 50), pygame.Rect(6520, 430, 50, 100), pygame.Rect(6620, 410, 200, 20), pygame.Rect(7040, 380, 50, 50), pygame.Rect(6860, 430, 50, 100),
+pygame.Rect(7300, 400, 50, 100), pygame.Rect(7250, 450, 50, 50)]
 
 towers = [Tower("classique", -600, -600), Tower("degats_de_zone", -1000, -450), Tower("classique", -1900, -1000), Tower("degats_de_zone",-2250, -425), 
 Tower("classique", -2740, -1000), Tower("classique", -2915, -1000), Tower("classique", -3500, -700), Tower("degats_de_zone", -4300, -300),
-Tower("degats_de_zone", -5000, -320), Tower("sniper", -5850, -350), Tower("degats_de_zone", -6125, -985)]
+Tower("degats_de_zone", -5000, -320), Tower("sniper", -5850, -350), Tower("degats_de_zone", -6125, -985), Tower("sniper", -6900, -985), Tower("sniper", -6600, -585),
+Tower("classique", -6700, -200)]
 
-traps = [Trap(-800, -1000), Trap(-2225, -935), Trap(-2175, -985), Trap(-3980, -640)]
+traps = [Trap(-800, -1000), Trap(-2225, -935), Trap(-2175, -985), Trap(-3980, -640), Trap(-6800, -720), Trap(-6400, -720)]
+
 spikes = [Spike(-2740, -910), Spike(-2915, -910), Spike(-4040, -980), Spike(-3680, -280), Spike(-3250, -1070), Spike(-3250, -1070), Spike(-3300, -1070),
 Spike(-3350, -1070), Spike(-3400, -1070), Spike(-3450, -1070), Spike(-3500, -1070), Spike(-3550, -1070), Spike(-3600, -1070), Spike(-3650, -1070), 
 Spike(-3700, -1070), Spike(-3750, -1070), Spike(-3800, -1070), Spike(-3850, -1070), Spike(-3900, -1070), Spike(-3950, -1070), Spike(-4000, -1070), 
@@ -28,4 +34,5 @@ Spike(-5550, -990), Spike(-5600, -990), Spike(-5650, -990), Spike(-5700, -990), 
 Spike(-5950, -990), Spike(-6000, -990), Spike(-5475, -600), Spike(-5525, -600), Spike(-5575, -600), Spike(-5835, -720), Spike(-5885, -720),Spike(-5935, -720), 
 Spike(-5985, -720), Spike(-6035, -720)]
 
-checkpoints=[(4750, 965)]
+checkpoints=[(4750, 965), (7500, 965)]
+
