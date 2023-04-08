3b82fe3cc28689866a864f2b88e7315783651ad3
Fraizard <94200448+Fraizard@users.noreply.github.com>
2023-04-08 20:48:14 +0200

    Create class_Base.py

---
+++ NeonTD/class_Base.py
@@ -0,0 +1,17 @@
+import pygame
+import math
+import time
+
+class Base(pygame.sprite.Sprite):
+    def __init__(self, x, y, image):
+        super().__init__()
+        self.image = pygame.image.load(image)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+    def remove(self):
+        self.kill()
