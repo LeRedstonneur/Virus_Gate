8e82d53218c751e8a895759e668f31bd6467beba
SLeotch <123469709+SLeotch@users.noreply.github.com>
2023-04-11 15:33:20 +0200

    Update obstacles.py

--- Chapitre_2/Code/obstacles.py
+++ Chapitre_2/Code/obstacles.py
@@ -16,8 +16,14 @@ pygame.Rect(5650, 690, 80, 20), pygame.Rect(5670, 840, 80, 20), pygame.Rect(5450
 pygame.Rect(5950, 920, 120, 20), pygame.Rect(6050, 860, 50, 150), pygame.Rect(6250, 750, 50, 150), pygame.Rect(6500, 865, 50, 50), pygame.Rect(6400, 965, 50, 50),
 pygame.Rect(6600, 750, 50, 150), pygame.Rect(6750, 950, 80, 20), pygame.Rect(6900, 880, 80, 20), pygame.Rect(7050, 950, 80, 20), pygame.Rect(7200, 880, 80, 20), 
 pygame.Rect(7050, 820, 80, 20), pygame.Rect(7350, 350, 50, 800), pygame.Rect(6300, 500, 1050, 50), pygame.Rect(6250, 650, 80, 20), pygame.Rect(6100, 550, 80, 20),
-pygame.Rect(6380, 380, 50, 50), pygame.Rect(6520, 430, 50, 100), pygame.Rect(6620, 410, 200, 20), pygame.Rect(7040, 380, 50, 50), pygame.Rect(6860, 430, 50, 100),
-pygame.Rect(7300, 400, 50, 100), pygame.Rect(7250, 450, 50, 50)]
+pygame.Rect(6380, 380, 50, 50), pygame.Rect(6520, 430, 50, 100), pygame.Rect(6620, 360, 200, 20), pygame.Rect(7040, 380, 50, 50), pygame.Rect(6860, 430, 50, 100),
+pygame.Rect(7300, 400, 50, 100), pygame.Rect(7250, 450, 50, 50), pygame.Rect(7900, 400, 50, 800), pygame.Rect(8300, 400, 50, 800), pygame.Rect(8500, 820, 350, 20),
+pygame.Rect(8500, 970, 350, 20), pygame.Rect(8830, 820, 20, 150), pygame.Rect(8850, 250, 80, 20), pygame.Rect(9175, 970, 80, 20), pygame.Rect(8650, 1080, 800, 50), 
+pygame.Rect(9275, 870, 80, 20), pygame.Rect(9375, 770, 80, 20), pygame.Rect(9475, 670, 80, 20), pygame.Rect(9575, 570, 80, 20), pygame.Rect(9675, 470, 80, 20),
+pygame.Rect(9375, 970, 80, 20), pygame.Rect(9475, 870, 80, 20), pygame.Rect(9575, 770, 80, 20), pygame.Rect(9675, 670, 80, 20), pygame.Rect(9775, 570, 80, 20),
+pygame.Rect(9875, 470, 80, 20), pygame.Rect(9575, 970, 80, 20), pygame.Rect(9675, 870, 80, 20), pygame.Rect(9775, 770, 80, 20), pygame.Rect(9875, 670, 80, 20), 
+pygame.Rect(9975, 570, 80, 20), pygame.Rect(10075, 470, 80, 20), pygame.Rect(9775, 970, 80, 20), pygame.Rect(9875, 870, 80, 20), pygame.Rect(9975, 770, 80, 20), 
+pygame.Rect(10075, 670, 80, 20), pygame.Rect(10175, 570, 80, 20), pygame.Rect(10275, 470, 80, 20)]
 
 towers = [Tower("classique", -600, -600), Tower("degats_de_zone", -1000, -450), Tower("classique", -1900, -1000), Tower("degats_de_zone",-2250, -425), 
 Tower("classique", -2740, -1000), Tower("classique", -2915, -1000), Tower("classique", -3500, -700), Tower("degats_de_zone", -4300, -300),
@@ -32,7 +38,9 @@ Spike(-3700, -1070), Spike(-3750, -1070), Spike(-3800, -1070), Spike(-3850, -107
 Spike(-4200, -630), Spike(-4625, -990), Spike(-5250, -990), Spike(-5300, -990), Spike(-5350, -990), Spike(-5400, -990), Spike(-5450, -990), Spike(-5500, -990),
 Spike(-5550, -990), Spike(-5600, -990), Spike(-5650, -990), Spike(-5700, -990), Spike(-5750, -990), Spike(-5800, -990), Spike(-5850, -990), Spike(-5900, -990),
 Spike(-5950, -990), Spike(-6000, -990), Spike(-5475, -600), Spike(-5525, -600), Spike(-5575, -600), Spike(-5835, -720), Spike(-5885, -720),Spike(-5935, -720), 
-Spike(-5985, -720), Spike(-6035, -720)]
+Spike(-5985, -720), Spike(-6035, -720), Spike(-7925, -380), Spike(-8325, -380), Spike(-8525, -800), Spike(-8725, -800), Spike(-8775, -800), Spike(-8825, -800),
+Spike(-8650, -950), Spike(-8700, -1070), Spike(-8750, -1070), Spike(-8800, -1070), Spike(-8850, -1070), Spike(-8900, -1070), Spike(-8950, -1070), 
+Spike(-9000, -1070), Spike(-9050, -1070), Spike(-9100, -1070), Spike(-9150, -1070), Spike(-9200, -1070), Spike(-9250, -1070), Spike(-9300, -1070), 
+Spike(-9350, -1070), Spike(-9400, -1070), Spike(-9450, -1070), Spike(-9500, -1070), Spike(-9550, -1070), Spike(-9600, -1070), Spike(-9650, -1070)]
 
-checkpoints=[(4750, 965), (7500, 965)]
-
+checkpoints=[(4750, 965), (7500, 965), (11000, 965)]
