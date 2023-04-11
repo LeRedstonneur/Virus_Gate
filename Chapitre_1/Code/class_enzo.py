e4d3ca69fd80a74289ef70a9bf9d8c938414df3c
Aertfly <123474051+Aertfly@users.noreply.github.com>
2023-04-11 12:21:25 +0200

    Update class_enzo.py

--- Chapitre_1/Code/class_enzo.py
+++ Chapitre_1/Code/class_enzo.py
@@ -173,7 +173,51 @@ class Base():
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
+        
+ class Base():
+    def __init__(self, x, y, image,width,height,index):
+        self.ratio = (index[0],index[1])
+        self.dimensions = (width,height)
+        self.x= x
+        self.y= y
+        self.image= pygame.image.load(image)
+        self.rect = pygame.Rect(x,y,width,height)
+        self.choice = False
+        self.tower_rect = []
 
+    def remove(self):
+        del self
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+    def resize(self,size) :
+        self.dimensions = size
+        width = size[0]
+        height = size[1]
+        self.x = width * self.ratio[0]
+        self.y = height * self.ratio[1]
+        self.rect = pygame.Rect(self.x,self.y,width,height)
+        
+
+    def chose(self,screen,tower_list):
+        centre = (self.x+(self.dimensions[0]//2),self.y+(self.dimensions[1]//2))
+        pygame.draw.circle(screen,(0,0,0),centre,self.dimensions[0],5)
+        dimensions_twr =  (self.dimensions[0]//1.5,self.dimensions[1]//1.5)
+        coord_twr = (centre[0]+self.dimensions[0]//2,centre[1]+self.dimensions[1]//2)
+        screen.blit(pygame.transform.scale(tower_list[0], dimensions_twr) , coord_twr)
+        self.tower_rect.append(pygame.Rect(dimensions_twr[0],dimensions_twr[1],coord_twr[0],coord_twr[1]))
+
+    def update(self,mouse_pos):
+        for rect in self.tower_rect :
+            if rect.collidepoint(mouse_pos): 
+                print("square")
+                
+        if self.rect.collidepoint(mouse_pos):
+            self.choice = True  
+        else : 
+            self.choice = False
+            self.tower_rect = []
 
 class Upgrade:
     def __init__(self):
