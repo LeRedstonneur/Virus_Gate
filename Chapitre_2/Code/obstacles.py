5618bb00592cf5b09fe4e530e6fc5a9e17b4bea8
SLeotch <123469709+SLeotch@users.noreply.github.com>
2023-04-15 16:50:22 +0200

    Update obstacles.py

--- Chapitre_2/Code/obstacles.py
+++ Chapitre_2/Code/obstacles.py
@@ -23,7 +23,8 @@ pygame.Rect(9275, 870, 80, 20), pygame.Rect(9375, 770, 80, 20), pygame.Rect(9475
 pygame.Rect(9375, 970, 80, 20), pygame.Rect(9475, 870, 80, 20), pygame.Rect(9575, 770, 80, 20), pygame.Rect(9675, 670, 80, 20), pygame.Rect(9775, 570, 80, 20),
 pygame.Rect(9875, 470, 80, 20), pygame.Rect(9575, 970, 80, 20), pygame.Rect(9675, 870, 80, 20), pygame.Rect(9775, 770, 80, 20), pygame.Rect(9875, 670, 80, 20), 
 pygame.Rect(9975, 570, 80, 20), pygame.Rect(10075, 470, 80, 20), pygame.Rect(9775, 970, 80, 20), pygame.Rect(9875, 870, 80, 20), pygame.Rect(9975, 770, 80, 20), 
-pygame.Rect(10075, 670, 80, 20), pygame.Rect(10175, 570, 80, 20), pygame.Rect(10275, 470, 80, 20)]
+pygame.Rect(10075, 670, 80, 20), pygame.Rect(10175, 570, 80, 20), pygame.Rect(10275, 470, 80, 20), pygame.Rect(10175, 870, 300, 20), pygame.Rect(10500, 770, 200, 20),
+pygame.Rect(10725, 670, 100, 20)]
 
 towers = [Tower("classique", -600, -600), Tower("degats_de_zone", -1000, -450), Tower("classique", -1900, -1000), Tower("degats_de_zone",-2250, -425), 
 Tower("classique", -2740, -1000), Tower("classique", -2915, -1000), Tower("classique", -3500, -700), Tower("degats_de_zone", -4300, -300),
@@ -41,6 +42,11 @@ Spike(-5950, -990), Spike(-6000, -990), Spike(-5475, -600), Spike(-5525, -600), 
 Spike(-5985, -720), Spike(-6035, -720), Spike(-7925, -380), Spike(-8325, -380), Spike(-8525, -800), Spike(-8725, -800), Spike(-8775, -800), Spike(-8825, -800),
 Spike(-8650, -950), Spike(-8700, -1070), Spike(-8750, -1070), Spike(-8800, -1070), Spike(-8850, -1070), Spike(-8900, -1070), Spike(-8950, -1070), 
 Spike(-9000, -1070), Spike(-9050, -1070), Spike(-9100, -1070), Spike(-9150, -1070), Spike(-9200, -1070), Spike(-9250, -1070), Spike(-9300, -1070), 
-Spike(-9350, -1070), Spike(-9400, -1070), Spike(-9450, -1070), Spike(-9500, -1070), Spike(-9550, -1070), Spike(-9600, -1070), Spike(-9650, -1070)]
+Spike(-9350, -1070), Spike(-9400, -1070), Spike(-9450, -1070), Spike(-9500, -1070), Spike(-9550, -1070), Spike(-9600, -1070), Spike(-9650, -1070),
+Spike(-9700, -1070), Spike(-9750, -1070), Spike(-9800, -1070), Spike(-9850, -1070), Spike(-9900, -1070), Spike(-9950, -1070), Spike(-10000, -1070), 
+Spike(-10050, -1070), Spike(-10100, -1070), Spike(-10150, -1070), Spike(-10200, -1070), Spike(-10250, -1070), Spike(-10300, -1070), Spike(-10350, -1070), 
+Spike(-10400, -1070), Spike(-10450, -1070), Spike(-10500, -1070), Spike(-10550, -1070), Spike(-10600, -1070), Spike(-10650, -1070), Spike(-10700, -1070), 
+Spike(-10750, -1070), Spike(-9815, -950), Spike(-9615, -950), Spike(-9415, -950), Spike(-9515, -650), Spike(-9715, -850), Spike(-9915, -850), 
+Spike(-9815, -550), Spike(-9815, -750), Spike(-9915, -650), Spike(-10115, -650), Spike(-10215, -550), Spike(-10315, -450), Spike(-10115, -450)]
 
 checkpoints=[(4750, 965), (7500, 965), (11000, 965)]
